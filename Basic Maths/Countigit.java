7a64ef86616f3da3973d1960a91f4211c7ce2d4a
Rohit danu <danurohit32@gmail.com>
2026-07-29 23:25:27 +0530

    problem 1 - B maths

---
+++ Basic Maths/Countigit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Countigit{
+
+    public static void main(String[] args){
+
+      Scanner sc= new Scanner(System.in);
+      int n = sc.nextInt();
+    
+      int count=0;
+
+       while(n>0){
+        count++;
+        n=n/10;// to remove last digit
+        }
+        
+System.out.println("Total Digits ="+count);
+sc.close();
+
+}
+}
+
